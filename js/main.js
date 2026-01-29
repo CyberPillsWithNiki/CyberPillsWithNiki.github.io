faec3764a03fca972ae126aa72aaed43b445719e
Nicoletta <cyberwithniki@gmail.com>
2026-01-29 11:47:37 +0000

    New Visuals, colours and picture

--- js/main.js
+++ js/main.js
@@ -2,3 +2,12 @@
 console.log("Portfolio loaded successfully!");
 
 // You can later add interactivity, e.g., filter projects by type (Cyber / AI / Data)
+const cards = document.querySelectorAll('.topic-card');
+
+cards.forEach(card => {
+    card.addEventListener('click', () => {
+        cards.forEach(c => c.style.transform = 'translateY(0)');
+        card.style.transform = 'translateY(-15px)';
+        card.style.boxShadow = '0 15px 25px rgba(88,166,255,0.5)';
+    });
+});
